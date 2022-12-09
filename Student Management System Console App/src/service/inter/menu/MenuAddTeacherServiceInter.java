e08d57cd961e2f5cd0445464b5e414c4c0fff63f
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-12-09 11:56:07 +0400

    Add files via upload

---
+++ Student Management System Console App/src/service/inter/menu/MenuAddTeacherServiceInter.java
@@ -0,0 +1,6 @@
+package service.inter.menu;
+
+import service.inter.SavableProcess;
+
+public interface MenuAddTeacherServiceInter extends SavableProcess {
+}
---
+++ Student Management System Console App/src/service/inter/menu/SavableProcess.java
@@ -0,0 +1,11 @@
+package service.inter;
+
+import bean.Config;
+
+public interface SavableProcess extends Process {
+    public default void process() {
+        processLogic();
+        Config.save();
+    }
+
+}
