1c551abbc318d57559d4395cf8c86bbdf4b896cc
Arturk Mammadli <erturkmemmedli@gmail.com>
2023-01-09 15:54:41 +0400

    Changes have been made to TCP Server/Client app

--- TCP Client Server App/TCPServer/src/main/java/tcpserver/TCPServer.java
+++ TCP Client Server App/TCPServer/src/main/java/tcpserver/TCPServer.java
@@ -3,11 +3,50 @@ package tcpserver;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.ByteBuffer;
 
 public class TCPServer {
     public static void main(String[] args) throws Exception {
-        readAsByte();
+        readRequestAndWriteResponse();
+    }
+
+    public static void readRequestAndWriteResponse() throws Exception {
+        int bufferSize = 8192;
+        ServerSocket ourFirstServerSocket = new ServerSocket(6789);
+        while (true) {
+            Socket connection = ourFirstServerSocket.accept();
+            DataInputStream dataStream = new DataInputStream(connection.getInputStream());
+            String result = readRequest(dataStream);
+            System.out.println(result);
+            OutputStream os = connection.getOutputStream();
+            DataOutputStream dos = new DataOutputStream(os);
+            String message = "Bir dusun bu menzereni. Doyur qapini pencereni. Keder seher, hicran her axsam";
+            writeResponse(dos, message);
+            connection.close();
+        }
+    }
+
+    public static String readRequest(InputStream is) throws Exception {
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br  = new BufferedReader(isr);
+        String message = "";
+        while (true) {
+            String s = br.readLine();
+            if (s == null || s.trim().length() == 0) break;
+            else message += s + "\r\n";
+            System.out.println(s);
+        }
+        return message;
+    }
+
+    public static void writeResponse(OutputStream dos, String s) throws Exception {
+        String response = "HTTP/1.1 200 OK\r\n"
+                + "Server: YarServer 2009-09-09\r\n"
+                + "Content-Type: text/html\r\n"
+                + "Content-Length: " + s.length() + "\r\n"
+                + "Connection: close\r\n\r\n";
+        String result = response + s;
+        dos.write(result.getBytes());
+        dos.flush();
     }
 
     public static void readAsByte() throws Exception {
